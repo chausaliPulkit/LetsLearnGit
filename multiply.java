365ffeab6b07ddfbec6af5c7a5bf952301478fff
Pulkit Chausali <pulkitchausali.22@gmail.com>
2021-11-25 18:07:51 +0530

    added multiply.java

---
+++ multiply.java
@@ -0,0 +1,10 @@
+public class multiply {
+    public static void main(String[] args) {
+        int a = 34;
+        int b = 43;
+
+        System.out.println("The multiply is "+(a*b));
+        
+    }
+    
+}
